825a90a053ccd0eac3b077bef313a23674f6cd68
Ishita Thapliyal <56544990+itta10@users.noreply.github.com>
2022-03-20 02:46:55 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,15 +2,16 @@
 $name= $_POST['name'];
 $e_mail= $_POST['e_mail'];
 $message=$_POST['message'];
+$phone=$_POST['phone'];
 //DATABASE connection
 
-$conn= new mysqli('localhost','root','','artportfolio');
+$conn= new mysqli(' ',' ','','');
 if($conn->connect_error){
   die('Connection Failed: ' .$conn->connect_error);
 }
 else{
-  $stmt= $conn->prepare("insert into contactme(name,e_mail,message) values(?,?,?)");
-$stmt->bind_param("sss",$name,$e_mail,$message);
+  $stmt= $conn->prepare("insert into contactme(name,e_mail,message,phone) values(?,?,?,?)");
+$stmt->bind_param("ssss",$name,$e_mail,$message,$phone);
 $stmt->execute();
 echo '<script>alert("Ish Wish")</script>';
 
